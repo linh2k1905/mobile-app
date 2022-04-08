c465c6f1003b47014534a697dde35b7b2f13700d
linh truong <thaolinhtruong20001905@gmail.com>
2022-04-08 19:40:44 +0700

    add image  for mobile app

--- screens/Home.js
+++ screens/Home.js
@@ -1,26 +1,68 @@
-import react from 'react-native'
-import { View, Text } from 'react-native'
-import { URL } from '../constants'
-const getAllHouseFromServer = () => {
-
-    fetch(
-        URL.LOCALHOST + '/api/get-all-home'
-    ).then(async (res) => {
-        let response = await res.json();
-        return response;
-    });
-
-
-
-}
+import { View, Text, Image, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { URL } from '../constants';
 const Home = () => {
+    const [home, setHome] = useState([]);
+    function getAllHouseFromServer() {
+
+        fetch(
+            URL.LOCALHOST + '/api/get-all-home-to-mobile'
+        ).then(async (res) => {
+            let response = await res.json();
+            let homes = response.data;
+            setHome(homes);
+
+
+        });
+
+
+
+        return (<View>
+            {home.length > 0 && home.map((item, index) => {
+                return (
+                    <View key={index}>
+                        <Text >{item.name}</Text>
+                        <Text >{item.price / 1000000}</Text>
+                        <Image
+
+                            source={{
+                                uri: item.image
+
+
+                            }}
+                            style={{ height: 200, width: 250 }}
+                        />
+                    </View>
+
+
+                )
+            })}
+        </View>
+        )
+    }
 
 
     return (
 
         <View>
-            <Text>Home</Text>
+
+            {getAllHouseFromServer()}
+
         </View>
     )
 }
+const styles = StyleSheet.create({
+    container: {
+        paddingTop: 50,
+    },
+    stretch: {
+        width: 200,
+        height: 200,
+        resizeMode: 'cover',
+        borderColor: 'red',
+        borderWidth: 1
+
+
+    },
+});
 export default Home;
