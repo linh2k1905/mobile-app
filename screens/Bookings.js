b2d37c637b05f4087ec6ca28e177fd014083803d
linh truong <thaolinhtruong20001905@gmail.com>
2022-04-12 19:47:03 +0700

    fix bookings house  in get all home

--- screens/Bookings.js
+++ screens/Bookings.js
@@ -1,9 +1,38 @@
 import { View, Text, SafeAreaView, StatusBar } from 'react-native'
 import { styles } from '../components/styles';
+import { useState } from 'react';
+import moment from 'moment';
 
 
 const Bookings = ({ route, navigation }) => {
     let { item } = route.params;
+
+    function setArrayTime() {
+        const allDays = [];
+        for (let i = 0; i <= 7; i++) {
+            let obj = {};
+            if (i === 0) {
+                let ddMM = moment(new Date()).format('DD/MM');
+                let today = `Today - ${ddMM}`;
+                obj.label = today;
+            }
+            else obj.label = moment(new Date()).add(i, 'days').locale('en').format('dddd - DD/MM');
+            obj.value = moment(new Date()).add(i, 'days').startOf('day').valueOf();
+            allDays.push(obj);
+
+        }
+
+        return (
+            allDays && allDays.length && allDays.map((item) => {
+                return (
+                    <View>
+                        <Text>{item.label}</Text>
+                    </View>
+                )
+            })
+
+        )
+    }
     return (
 
         <SafeAreaView
@@ -15,7 +44,10 @@ const Bookings = ({ route, navigation }) => {
                 <Text>Địa chỉ: {item.address}</Text>
                 <Text>Sđt chủ trọ: {item.User.tel}</Text>
                 <Text>Email: {item.User.email}</Text>
+
             </View>
+            {setArrayTime()}
+
         </SafeAreaView>
     )
 }
